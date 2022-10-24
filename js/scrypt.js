9ae8977a4f8b1578ee262443133d484dfd37f22b
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-24 15:56:38 +0200

    aggiunge piccole animazioni all'hover

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,16 +1,17 @@
 'strict mode'
 
-// 1 definire l'elemento che contiene il valore
 
-
-// 2 cicliamo i numeri da 1 a 100
+// cicliamo i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
         console.log(i);
+        // ora che abbiamo i valori selezioniamo il container e creiamo le caselle
         let containerElement = document.querySelector("div.container");
         let containednumber = document.createElement("div");
+        // ora aggiungiamo la classe alle caselle create e ci appendiamo il numero ciclato
         containednumber.classList.add('number');
         containednumber.append(i);
+        // al container selezionato ora appendiamo le caselle con il suo numero all'interno
         containerElement.append(containednumber);
 
     // mettiamo le condizioni varie per inserire i fizz e i buzz
